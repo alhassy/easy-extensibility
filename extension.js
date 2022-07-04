39bebff49599372881d9e0b83b8a8dd4ffc7555d
Musa Al-hassy <musa@weeverapps.com>
2022-07-04 12:52:36 -0400

    Add: Two HelloWorld functions, with keybindings

--- extension.js
+++ extension.js
@@ -1,36 +1,14 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
+	context.subscriptions.push(vscode.commands.registerCommand('easy-extensibility.evaluateSelection',
+		() => vscode.window.showInformationMessage('Hello World from easy-extensibility, eh 1!')));
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "easy-extensibility" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('easy-extensibility.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from easy-extensibility, eh!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('easy-extensibility.executeRegisteredCommand',
+		() => vscode.window.showInformationMessage('Hello World from easy-extensibility, eh 2!')));
 }
-
-// this method is called when your extension is deactivated
-function deactivate() {}
 
 module.exports = {
 	activate,
-	deactivate
+	deactivate: () => {}
 }
